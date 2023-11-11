5c0e36ac9bd4ae452040ab35048707678be76f96
David Santos <79118845+dvctrl@users.noreply.github.com>
2023-11-11 19:03:36 -0300

    Update iac.sh

--- iac.sh
+++ iac.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-echo "Criando diretórios..."
+echo " :::::: CONFIGURANDO DIRETORIOS ::::::"" 
 
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando grupos de usuários..."
-
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
+echo ":::::: CONFIGURANDO USUARIOS ::::::"
 
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
@@ -27,8 +25,6 @@ useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
-
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
@@ -38,4 +34,4 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "Fim....."
+echo " :::::: SCRIPT EXECUTADO COM SUCESSO ::::::"
